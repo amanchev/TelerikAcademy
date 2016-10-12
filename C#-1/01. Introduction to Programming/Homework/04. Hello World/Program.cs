5b296b2ceb7e46e74a39f8f0b5273abfda91a143
askata992 <askata9213@gmail.com>
2016-10-12 11:53:54 +0300

    add C#-1 to courses

---
+++ C#-1/01. Introduction to Programming/Homework/04. Hello World/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, C#!");
+    }
+}
---
+++ C#-1/01. Introduction to Programming/Homework/15. Age/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class PrintAge
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime Birthday = DateTime.ParseExact(Console.ReadLine(), "MM.dd.yyyy", null);
+            DateTime now = System.DateTime.Now;
+            int age = now.Year - Birthday.Year;
+            if (now.Month < Birthday.Month || (now.Month == Birthday.Month && now.Date < Birthday.Date))
+            {
+                age--;
+            }
+            Console.WriteLine(age);
+            Console.WriteLine(age + 10);
+        }
+    }
