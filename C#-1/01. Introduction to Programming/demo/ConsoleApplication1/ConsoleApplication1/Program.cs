9aa68927b1082127051e8a915404092e7d06a56f
askata992 <askata9213@gmail.com>
2016-11-15 10:40:03 +0200

    Add homeworks

---
+++ C#-1/01. Introduction to Programming/demo/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Main()
+    {
+
+
+
+
+
+
+
+        
+
+        int number = 5 / 2;
+        Console.WriteLine(number);
+
+
+
+    }
+}
+
---
+++ C#-1/01. Introduction to Programming/demo/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter X");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x>y)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
